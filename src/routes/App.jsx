7ef09af194e41e9744ee199389e06658d2175096
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-01-26 15:33:50 -0500

    Login comp moved from containers to pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from '../containers/Layout';
-import { Login } from '../containers/Login';
+import { Login } from '../pages/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoveryPassword } from '../containers/RecoveryPassword';
 import { Home } from '../pages/Home';
