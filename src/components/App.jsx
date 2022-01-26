d74bc9e47b9d865aa663e284e13783c69ab5724a
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-01-26 09:58:23 -0500

    recoveryPassword comp added & react-router-dom installed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Layout } from '../containers/Layout';
 import { Login } from '../containers/Login';
+import { RecoveryPassword } from '../containers/RecoveryPassword';
 import "../styles/global.css";
 
 export const App = () => {
   return (
     <Layout>
       <Login />
+      <RecoveryPassword />
     </Layout>
   )
 };
