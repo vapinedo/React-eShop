4afd13e8b56100b079420216801de5427e45b83b
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-01-31 15:48:09 -0500

    useEffect added to ProductList to fectch API Data & babel plugin added for async await

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProductItem } from '@components/ProductItem';
+import axios from "axios";
 import '@styles/ProductList.scss';
 
+const API = "https://api.escuelajs.co/api/v1/products";
+	
 export const ProductList = () => {
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(async() => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, []);
+	
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
